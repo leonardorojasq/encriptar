d8265da8ab75380e0f5f1be19b829b7b8141f5af
Leonardo Rojas Quevedo <leonardorojasq@outlook.com>
2022-12-14 20:15:51 -0500

    algo

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import Crypto
 import Crypto.PublicKey import RSA
 
 random_generator = Crypto.Random.new().read
+private_key = RSA.generate(random_generator)
+
+print(random_generator)
+print(private_key)
+
